ba98a9e7e9a08f9177244de009d16a73c3f3f85a
Max Görner <5477952+MaxG87@users.noreply.github.com>
2023-08-30 22:55:04 +0200

    feat(cli): Add first two CLI commands

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,3 +8,27 @@ def test_cli():
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
     assert "Show this message and exit." in result.output
+
+
+def test_open():
+    result = runner.invoke(app, ["open"])
+    assert result.exit_code == 0
+    assert result.output.startswith("Opening")
+
+
+def test_open_with_argument(tmp_path):
+    result = runner.invoke(app, ["open", str(tmp_path)])
+    assert result.exit_code == 0
+    assert result.output.endswith(f"{tmp_path}...\n")
+
+
+def test_close():
+    result = runner.invoke(app, ["close"])
+    assert result.exit_code == 0
+    assert result.output.startswith("Closing")
+
+
+def test_close_with_argument(tmp_path):
+    result = runner.invoke(app, ["close", str(tmp_path)])
+    assert result.exit_code == 0
+    assert result.output.endswith(f"{tmp_path}...\n")
